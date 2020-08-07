1a8a374e24a643d6cb10cfca30edabdc6c5fdbdd
Justin Langlinais <justinclanglinais@gmail.com>
2020-08-07 11:23:19 -0700

    troubleshooting AWS S3 for img upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
     end
     private
     def post_params
-        params.require(:post).permit(:image)
+        params.require(:post).permit(:image, :image_cache)
     end
 
 
